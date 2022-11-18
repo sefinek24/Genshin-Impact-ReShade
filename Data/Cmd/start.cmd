18057b8d713a1fcae816871346e8a981fed8db72
Sefinek <sefinek5916@gmail.com>
2022-11-18 11:51:06 +0100

    SV_15112022_120-003

--- Data/Cmd/start.cmd
+++ Data/Cmd/start.cmd
@@ -82,15 +82,15 @@ echo [✓] Done. Everything is ready (=^･ω･^=)! & echo.
 echo 6/8 - Starting...
 set /p RunID=< "%AppData%\Genshin Impact MP by Sefinek\run"
 if %RunID% == 1 (
-    echo [i] Click 'Yes' in the two UAC (User Account Control) notifications. File inject.exe and unlockfps_clr.exe. & echo.
+    echo [i] Click 'Yes' in the two UAC [User Account Control] notifications. File inject.exe and unlockfps_clr.exe. & echo.
     powershell.exe Data\PowerShell\mod.ps1
     call Data\Cmd\start\1.cmd
 ) else if %RunID% == 2 (
-    echo [i] Click 'Yes' in the UAC (User Account Control) notification. File inject.exe. & echo.
+    echo [i] Click 'Yes' in the UAC [User Account Control] notification. File inject.exe. & echo.
     powershell.exe Data\PowerShell\only-reshade.ps1
     call Data\Cmd\start\2.cmd
 ) else if %RunID% == 3 (
-    echo [i] Click 'Yes' in the UAC (User Account Control) notification. File unlockfps_clr.exe. & echo.
+    echo [i] Click 'Yes' in the UAC [User Account Control] notification. File unlockfps_clr.exe. & echo.
     powershell.exe Data\PowerShell\only-fps_unlocker.ps1
     call Data\Cmd\start\3.cmd
 ) else (
