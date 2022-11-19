4bcbee78555208e5dc1f691b10bae179534de26c
Sefinek <sefinek5916@gmail.com>
2022-11-19 14:41:19 +0100

    v2.0.0

--- Data/Cmd/start/error.cmd
+++ Data/Cmd/start/error.cmd
@@ -3,6 +3,6 @@ echo ===========================================================================
 echo [x] Oh, sorry. For some reason, the application failed to start.
 echo [i] If you need help, please create a new Issue on GitHub or add me on Discord. & echo.
 echo [i] My username: Sefinek#0001
-echo [i] GitHub: https://github.com/sefinek24/genshin-impact-reshade-2023/issues
+echo [i] GitHub: https://github.com/sefinek24/Genshin-Impact-ReShade/issues
 
 timeout /t 24 /nobreak
