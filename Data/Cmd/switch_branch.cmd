33b89c02fea86fbb8479e063e2ccbe32e3e4df15
Sefinek <sefinek5916@gmail.com>
2022-12-04 22:11:33 +0100

    Create switch_branch.cmd

---
+++ Data/Cmd/switch_branch.cmd
@@ -0,0 +1,58 @@
+@echo off
+title Switch branch - Genshin Impact Mod Pack 2023
+chcp 65001 > nul
+echo.⠀   ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⡶⢶⣦⡀
+echo.⠀  ⠀⠀⣴⡿⠟⠷⠆⣠⠋⠀⠀⠀⢸⣿
+echo.⠀   ⠀⣿⡄⠀⠀⠀⠈⠀⠀⠀⠀⣾⡿                           Genshin Impact ReShade 2023 Mod Pack
+echo.   ⠀⠀⠹⣿⣦⡀⠀⠀⠀⠀⢀⣾⣿                                       Switch branch
+echo.⠀   ⠀⠀⠈⠻⣿⣷⣦⣀⣠⣾⡿
+echo.    ⠀⠀⠀⠀⠀⠉⠻⢿⡿⠟
+echo. ⠀   ⠀⠀⠀⠀⠀⠀⡟⠀⠀⠀⢠⠏⡆⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⡀
+echo. ⠀   ⠀⠀⡟⢦⡀⠇⠀⠀⣀⠞⠀⠀⠘⡀⢀⡠⠚⣉⠤⠂⠀⠀⠀⠈⠙⢦⡀
+echo.   ⠀⠀⠀⠀⡇⠀⠉⠒⠊⠁⠀⠀⠀⠀⠀⠘⢧⠔⣉⠤⠒⠒⠉⠉⠀⠀⠀⠀⠹⣆      * Mod version: v3.0.0 [SV_DEV_04122022_300X-001]
+echo.    ⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⣤⠶⠶⢶⡄⠀⠀⠀⠀⢹⡆    * ReShade version: v5.5.1
+echo.   ⣀⠤⠒⠒⢺⠒⠀⠀⠀⠀⠀⠀⠀⠀⠤⠊⠀⢸⠀⡿⠀⡀⠀⣀⡟⠀⠀⠀⠀⢸⡇     * FPS unlocker version: v2.0.5
+echo.  ⠈⠀⠀⣠⠴⠚⢯⡀⠐⠒⠚⠉⠀⢶⠂⠀⣀⠜⠀⢿⡀⠉⠚⠉⠀⠀⠀⠀⣠⠟
+echo.   ⠠⠊⠀⠀⠀⠀⠙⠂⣴⠒⠒⣲⢔⠉⠉⣹⣞⣉⣈⠿⢦⣀⣀⣀⣠⡴⠟
+echo ========================================================================================= & echo.
+
+
+echo 1/3 - git branch --show-current
+git rev-parse --abbrev-ref HEAD
+set branch=
+for /F "delims=" %%n in ('git branch --show-current') do set "branch=%%n"
+if "%branch%"=="" echo Not a git branch! && goto :EOF
+echo.
+
+if "%branch%" == "main" (
+    echo 2/3 - Git checkout dev
+    git checkout dev
+) else if "%branch%" == "dev" (
+    echo 2/3 - Git checkout main
+    git checkout main
+) else (
+    echo Unknown branch.
+    goto nothing_to_do
+)
+echo.
+
+echo 3/3 - Changing branch...
+if "%branch%" == "main" (
+    git pull . origin/dev
+    echo. && echo.
+    echo [i] 🎉 You are in main branch.
+) else if "%branch%" == "dev" (
+    git pull . origin/main
+    echo. && echo.
+    echo [i] 🎉 You are in dev branch. !! BETA RELEASES !!
+) else (
+    echo Unknown branch.
+    goto nothing_to_do
+)
+echo.
+
+echo [i] Success. You can close this window.
+
+:nothing_to_do
+set /p 0=
+exit
