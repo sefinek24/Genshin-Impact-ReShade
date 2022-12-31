a74bc4be1c9ee9a4be1a944e90231729dd735c90
Sefinek <sefinek5916@gmail.com>
2022-12-31 05:38:07 +0100

    BV_20122022_301.02

--- Data/Web/api/local.js
+++ Data/Web/api/local.js
@@ -1,7 +1,7 @@
 const local = {
-	version: '3.0.1.0',
-	releaseVersion: '20.12.2022',
-	lastUpdate: '20.12.2022',
-	build: '20122022_301.01',
+	version: '3.0.1.1',
+	releaseVersion: '30.12.2022',
+	lastUpdate: '30.12.2022',
+	build: '20122022_301.02',
 	beta: true,
 };
