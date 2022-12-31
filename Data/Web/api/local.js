cf14bb1ffc06a6ddc86b0c1ea8124ea244cfd221
Sefinek <sefinek5916@gmail.com>
2022-12-31 05:45:27 +0100

    Whoops wrong date :D

--- Data/Web/api/local.js
+++ Data/Web/api/local.js
@@ -1,7 +1,7 @@
 const local = {
 	version: '3.0.1.1',
-	releaseVersion: '30.12.2022',
-	lastUpdate: '30.12.2022',
+	releaseVersion: '31.12.2022',
+	lastUpdate: '31.12.2022',
 	build: '20122022_301.02',
 	beta: true,
 };
