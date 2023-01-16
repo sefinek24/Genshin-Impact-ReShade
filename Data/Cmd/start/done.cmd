f9f6e8f100d6a373d11088206771a9ec16a335f2
Sefinek <sefinek5916@gmail.com>
2023-01-16 17:53:32 +0100

    Delete useless cmd files

--- Data/Cmd/start/done.cmd
+++
@@ -1,8 +0,0 @@
-echo.
-echo ========================================================================================= & echo.
-echo [✓] Done! If you need help, add me on Discord Sefinek#0001.
-echo [i] Visit my YouTube channel. Thank you.
-echo [i] https://www.youtube.com/channel/UClrAIcAzcqIMbvGXZqK7e0A
-
-timeout /t 19 /nobreak
-exit
--- Data/Cmd/start/error.cmd
+++
@@ -1,8 +0,0 @@
-echo.
-echo ========================================================================================= & echo.
-echo [x] Oh, sorry. For some reason, the application failed to start.
-echo [i] If you need help, please create a new Issue on GitHub or add me on Discord. & echo.
-echo [i] My username: Sefinek#0001
-echo [i] GitHub: https://github.com/sefinek24/Genshin-Impact-ReShade/issues
-
-timeout /t 30 /nobreak
