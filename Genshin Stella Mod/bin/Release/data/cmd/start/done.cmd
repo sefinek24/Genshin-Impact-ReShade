9f2089062c344e3f93a178f11962b7bf4b7c057b
sefinek24 <sefinek5916@gmail.com>
2023-05-22 04:26:46 +0200

    dsfasdf

--- Genshin Stella Mod/bin/Release/data/cmd/start/done.cmd
+++
@@ -1,7 +0,0 @@
-echo.
-echo ========================================================================================= & echo.
-echo [✓] Done! If you need help, visit: https://sefinek.net/genshin-impact-reshade/support
-echo [i] Check my YouTube channel: https://www.youtube.com/@sefinek
-
-timeout /t 9 /nobreak
-exit
