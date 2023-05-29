f3115f5dad23262e0cdf50d27269ace0349ec946
sefinek24 <sefinek5916@gmail.com>
2023-05-29 11:33:50 +0200

    Part 2.2

--- First app launch/Forms/Window.Designer.cs
+++ First app launch/Forms/Window.Designer.cs
@@ -47,229 +47,133 @@ namespace ConfWindow.Forms
             // 
             // linkLabel1
             // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
             this.linkLabel1.ActiveLinkColor = System.Drawing.Color.CornflowerBlue;
-            this.linkLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
             this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.linkLabel1.Font = new System.Drawing.Font("Liberation Serif", 42.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
             this.linkLabel1.LinkColor = System.Drawing.Color.LightSkyBlue;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 553);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(1121, 63);
-            this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = ">> I am ready to continue... <<";
-            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LetsGo_LinkClicked);
             // 
             // MadeBySefinek
             // 
-            this.MadeBySefinek.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.MadeBySefinek.AutoSize = true;
+            resources.ApplyResources(this.MadeBySefinek, "MadeBySefinek");
             this.MadeBySefinek.BackColor = System.Drawing.Color.Transparent;
-            this.MadeBySefinek.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MadeBySefinek.ForeColor = System.Drawing.Color.MediumBlue;
-            this.MadeBySefinek.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.MadeBySefinek.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.MadeBySefinek.Location = new System.Drawing.Point(931, 51);
             this.MadeBySefinek.Name = "MadeBySefinek";
-            this.MadeBySefinek.Size = new System.Drawing.Size(202, 33);
-            this.MadeBySefinek.TabIndex = 78;
-            this.MadeBySefinek.Text = "made by Sefinek";
-            this.MadeBySefinek.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label1
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
+            resources.ApplyResources(this.label1, "label1");
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.MediumBlue;
-            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label1.Location = new System.Drawing.Point(682, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(451, 45);
-            this.label1.TabIndex = 77;
-            this.label1.Text = "Genshin Impact Stella Mod";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // checkBox1
             // 
-            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox1.AutoSize = true;
+            resources.ApplyResources(this.checkBox1, "checkBox1");
             this.checkBox1.BackColor = System.Drawing.Color.Transparent;
             this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox1.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox1.Location = new System.Drawing.Point(26, 410);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(334, 24);
-            this.checkBox1.TabIndex = 81;
-            this.checkBox1.Text = "Install or update Windows Terminal *";
             this.checkBox1.UseVisualStyleBackColor = false;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.InstOrUpdWT_CheckedChanged);
             // 
             // checkBox2
             // 
-            this.checkBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox2.AutoSize = true;
+            resources.ApplyResources(this.checkBox2, "checkBox2");
             this.checkBox2.BackColor = System.Drawing.Color.Transparent;
             this.checkBox2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox2.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox2.Location = new System.Drawing.Point(26, 266);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(316, 24);
-            this.checkBox2.TabIndex = 82;
-            this.checkBox2.Text = "Create new shortcuts on Desktop *";
             this.checkBox2.UseVisualStyleBackColor = false;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.NewShortcutsOnDesktop_CheckedChanged);
             // 
             // checkBox3
             // 
-            this.checkBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox3.AutoSize = true;
+            resources.ApplyResources(this.checkBox3, "checkBox3");
             this.checkBox3.BackColor = System.Drawing.Color.Transparent;
             this.checkBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox3.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox3.Location = new System.Drawing.Point(26, 290);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(352, 24);
-            this.checkBox3.TabIndex = 83;
-            this.checkBox3.Text = "Create internet shortcuts in start menu";
             this.checkBox3.UseVisualStyleBackColor = false;
             this.checkBox3.CheckedChanged += new System.EventHandler(this.InternetShortcutsInStartMenu_CheckedChanged);
             // 
             // checkBox4
             // 
-            this.checkBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox4.AutoSize = true;
+            resources.ApplyResources(this.checkBox4, "checkBox4");
             this.checkBox4.BackColor = System.Drawing.Color.Transparent;
             this.checkBox4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox4.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox4.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox4.Location = new System.Drawing.Point(26, 338);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(236, 24);
-            this.checkBox4.TabIndex = 84;
-            this.checkBox4.Text = "Update ReShade config *";
             this.checkBox4.UseVisualStyleBackColor = false;
             this.checkBox4.CheckedChanged += new System.EventHandler(this.UpdateReShadeConfig_CheckedChanged);
             // 
             // checkBox5
             // 
-            this.checkBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox5.AutoSize = true;
+            resources.ApplyResources(this.checkBox5, "checkBox5");
             this.checkBox5.BackColor = System.Drawing.Color.Transparent;
             this.checkBox5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox5.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox5.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox5.Location = new System.Drawing.Point(26, 362);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(274, 24);
-            this.checkBox5.TabIndex = 85;
-            this.checkBox5.Text = "Update FPS Unlocker config *";
             this.checkBox5.UseVisualStyleBackColor = false;
             this.checkBox5.CheckedChanged += new System.EventHandler(this.UpdateFpsUnlockerConfig_CheckedChanged);
             // 
             // checkBox6
             // 
-            this.checkBox6.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox6.AutoSize = true;
+            resources.ApplyResources(this.checkBox6, "checkBox6");
             this.checkBox6.BackColor = System.Drawing.Color.Transparent;
             this.checkBox6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox6.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox6.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox6.Location = new System.Drawing.Point(26, 386);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(227, 24);
-            this.checkBox6.TabIndex = 86;
-            this.checkBox6.Text = "Delete ReShade cache *";
             this.checkBox6.UseVisualStyleBackColor = false;
             this.checkBox6.CheckedChanged += new System.EventHandler(this.DeleteReShadeCache_CheckedChanged);
             // 
             // label2
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.label2, "label2");
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Ink Free", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label2.Location = new System.Drawing.Point(12, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1121, 89);
-            this.label2.TabIndex = 87;
-            this.label2.Text = "Welcome to the configuration window";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.label3, "label3");
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label3.Location = new System.Drawing.Point(12, 178);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1121, 62);
-            this.label3.TabIndex = 88;
-            this.label3.Text = resources.GetString("label3.Text");
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // checkBox7
             // 
-            this.checkBox7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.checkBox7.AutoSize = true;
+            resources.ApplyResources(this.checkBox7, "checkBox7");
             this.checkBox7.BackColor = System.Drawing.Color.Transparent;
             this.checkBox7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.checkBox7.Font = new System.Drawing.Font("UD Digi Kyokasho NK-R", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.checkBox7.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox7.Location = new System.Drawing.Point(26, 314);
             this.checkBox7.Name = "checkBox7";
-            this.checkBox7.Size = new System.Drawing.Size(483, 24);
-            this.checkBox7.TabIndex = 89;
-            this.checkBox7.Text = "Download or update shaders, default presets, addons *";
             this.checkBox7.UseVisualStyleBackColor = false;
             this.checkBox7.CheckedChanged += new System.EventHandler(this.DownloadOrUpdateShaders);
             // 
             // label4
             // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            resources.ApplyResources(this.label4, "label4");
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label4.Location = new System.Drawing.Point(12, 493);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(1121, 56);
-            this.label4.TabIndex = 90;
-            this.label4.Text = "You should keep in mind that previous configurations may be lost, including ReSha" +
-    "de and FPS Unlocker.\r\nHowever, your presets and other custom shaders will not be" +
-    " removed.";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label5
             // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label5.AutoSize = true;
+            resources.ApplyResources(this.label5, "label5");
             this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label5.Location = new System.Drawing.Point(22, 436);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(959, 23);
-            this.label5.TabIndex = 91;
-            this.label5.Text = "Fields marked with an asterisk (*) are recommended to be filled. If you are unsur" +
-    "e of what you\'re doing, do not uncheck any field.";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Window
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ConfWindow.Properties.Resources.bg;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1145, 632);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.checkBox7);
@@ -289,8 +193,6 @@ namespace ConfWindow.Forms
             this.MaximizeBox = false;
             this.Name = "Window";
             this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Prepare Stella Mod Launcher";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
