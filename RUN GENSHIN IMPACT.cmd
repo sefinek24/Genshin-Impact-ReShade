0b6a99be5c89360ea20c3e2c7e3c480d0495f589
Sefinek <sefinek5916@gmail.com>
2023-09-16 15:06:39 +0200

    Add 'RUN GENSHIN IMPACT.cmd'

---
+++ RUN GENSHIN IMPACT.cmd
@@ -0,0 +1,22 @@
+@echo off
+title Outdated method: Genshin Stella Mod
+
+echo ------------------------------ This injection method is outdated ------------------------------
+echo.
+echo If you used the 'git clone [Repository Link]' command, you've downloaded the source code of
+echo our software in this case. Compiling the code on your machine may not be possible because some
+echo files are not publicly available for security reasons.
+echo.
+echo If you want to install the Genshin Stella Mod, please visit https://genshin.sefinek.net
+echo and click the 'Download' button.
+echo.
+echo I also want to inform you that the YouTube video titled
+echo "How to install Genshin Impact Reshade Mod to get better graphics quality and unlock frames? 2022" is outdated.
+echo.
+echo ~ Good luck!
+
+goto done
+
+:done
+    pause >nul
+    goto done
