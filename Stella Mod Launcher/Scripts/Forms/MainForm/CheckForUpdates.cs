7bc9ccd145bfc1dfa495778d95d3fe88266287be
Sefinek <contact@sefinek.net>
2023-10-26 14:43:38 +0200

    Update CheckForUpdates.cs

--- Stella Mod Launcher/Scripts/Forms/MainForm/CheckForUpdates.cs
+++ Stella Mod Launcher/Scripts/Forms/MainForm/CheckForUpdates.cs
@@ -2,7 +2,6 @@ using System;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Taskbar;
