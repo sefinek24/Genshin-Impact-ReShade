5bf4e8ab3d2498d77715709d4f2a46ab3178f625
Sefinek <contact@sefinek.net>
2023-11-08 04:19:17 +0100

    7.8.1.3

--- MessageBOX48793142/MainWindow.Designer.cs
+++ MessageBOX48793142/MainWindow.Designer.cs
@@ -31,6 +31,9 @@ namespace InformationWindow
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             label1 = new Label();
             label2 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -39,11 +42,11 @@ namespace InformationWindow
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 33F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(35, 118);
+            label1.Location = new Point(342, 101);
             label1.Name = "label1";
-            label1.Size = new Size(913, 68);
+            label1.Size = new Size(484, 68);
             label1.TabIndex = 0;
-            label1.Text = "ATTENTION!";
+            label1.Text = "ATTENTION ^-^";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // label2
@@ -52,12 +55,34 @@ namespace InformationWindow
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(12, 186);
+            label2.Location = new Point(12, 212);
             label2.Name = "label2";
-            label2.Size = new Size(959, 225);
+            label2.Size = new Size(960, 225);
             label2.TabIndex = 1;
             label2.Text = resources.GetString("label2.Text");
             label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.angry_paimon;
+            pictureBox1.Location = new Point(189, 76);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(113, 114);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.Location = new Point(409, 471);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Click here if you like cats";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += MeowButton_Click;
             // 
             // MainWindow
             // 
@@ -65,15 +90,17 @@ namespace InformationWindow
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             BackgroundImageLayout = ImageLayout.None;
-            ClientSize = new Size(983, 528);
+            ClientSize = new Size(984, 506);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             Name = "MainWindow";
-            Opacity = 0.8D;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "ATTENTION";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -81,5 +108,7 @@ namespace InformationWindow
 
         private Label label1;
         private Label label2;
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
--- MessageBOX48793142/MainWindow.cs
+++ MessageBOX48793142/MainWindow.cs
@@ -1,66 +1,91 @@
-using System;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
+using NAudio.Wave;
 using Timer = System.Windows.Forms.Timer;
 
-namespace InformationWindow
+namespace InformationWindow;
+
+public partial class MainWindow : Form
 {
-    public partial class MainWindow : Form
+    private const int HwndTopmost = -1;
+    private const uint SwpNoSize = 0x0001;
+    private const uint SwpNoMove = 0x0002;
+    private readonly Timer _timer;
+    private int _displayCount;
+
+    public MainWindow()
     {
-        private readonly Timer _timer;
-        private int _displayCount;
+        InitializeComponent();
 
-        [DllImport("user32.dll")]
-        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        _timer = new Timer();
+        _timer.Interval = 1000;
+        _timer.Tick += Timer_Tick;
 
-        private const int HwndTopmost = -1;
-        private const uint SwpNoSize = 0x0001;
-        private const uint SwpNoMove = 0x0002;
+        FormBorderStyle = FormBorderStyle.None;
+        WindowState = FormWindowState.Maximized;
+        TopMost = true;
+        ShowInTaskbar = false;
 
-        public MainWindow()
+        _timer.Start();
+
+        Timer autoCloseTimer = new();
+        autoCloseTimer.Interval = 10000;
+        autoCloseTimer.Tick += AutoCloseTimer_Tick;
+        autoCloseTimer.Start();
+    }
+
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        if (_displayCount < 3)
         {
-            InitializeComponent();
-
-            _timer = new Timer();
-            _timer.Interval = 1000;
-            _timer.Tick += Timer_Tick;
-
-            FormBorderStyle = FormBorderStyle.None;
-            WindowState = FormWindowState.Maximized;
-            TopMost = true;
-            ShowInTaskbar = false;
-
-            _timer.Start();
-
-            Timer autoCloseTimer = new();
-            autoCloseTimer.Interval = 10000;
-            autoCloseTimer.Tick += AutoCloseTimer_Tick;
-            autoCloseTimer.Start();
+            _displayCount++;
         }
-
-        private void Timer_Tick(object sender, EventArgs e)
+        else
         {
-            if (_displayCount < 3)
+            _timer.Stop();
+            _timer.Dispose();
+        }
+    }
+
+    private static void AutoCloseTimer_Tick(object sender, EventArgs e)
+    {
+        Application.Exit();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+
+        SetWindowPos(Handle, HwndTopmost, 0, 0, 0, 0, SwpNoMove | SwpNoSize);
+    }
+
+    // Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47485">Pixabay</a>
+    private async void MeowButton_Click(object sender, EventArgs e)
+    {
+        Random random = new();
+        int randomNumber = random.Next(1, 5);
+        string mp3FilePath = @$"sound\meow{randomNumber}.mp3";
+
+        try
+        {
+            using (AudioFileReader audioFile = new(mp3FilePath))
+            using (WaveChannel32 volumeStream = new(audioFile))
             {
-                _displayCount++;
-            }
-            else
-            {
-                _timer.Stop();
-                _timer.Dispose();
+                volumeStream.Volume = 0.3f;
+                using (WaveOutEvent outputDevice = new())
+                {
+                    outputDevice.Init(volumeStream);
+                    outputDevice.Play();
+
+                    await Task.Delay(TimeSpan.FromSeconds(audioFile.TotalTime.TotalSeconds)).ConfigureAwait(false);
+                }
             }
         }
-
-        private static void AutoCloseTimer_Tick(object sender, EventArgs e)
+        catch (Exception ex)
         {
-            Application.Exit();
-        }
-
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-
-            SetWindowPos(Handle, HwndTopmost, 0, 0, 0, 0, SwpNoMove | SwpNoSize);
+            MessageBox.Show(ex.Message);
         }
     }
 }
