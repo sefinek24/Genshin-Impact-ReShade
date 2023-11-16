9c39453ec3bc20d5bddbaa1c2c331b144b9841a0
Sefinek <contact@sefinek.net>
2023-11-16 22:43:59 +0100

    Revert "Logger.WithProperty"
    This reverts commit a9f4ec13

--- Stella Mod Launcher/Program.cs
+++ Stella Mod Launcher/Program.cs
@@ -22,20 +22,18 @@ namespace StellaLauncher
     {
         // App
         public static readonly string AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        private static readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
-        public static readonly string AppNameVer = $"{AppName} • v{AppVersion}";
+        public static readonly string AppName = $"{Assembly.GetExecutingAssembly().GetName().Name} • v{AppVersion}";
         private static readonly string AppWebsiteSub = "https://genshin.sefinek.net";
         public static readonly string AppWebsiteFull = "https://sefinek.net/genshin-impact-reshade";
 
         // Files and folders
-        public static readonly string AppPath = AppDomain.CurrentDomain.BaseDirectory;
+        public static readonly string AppPath = AppContext.BaseDirectory;
         public static readonly string AppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Stella Mod Launcher");
         public static readonly string PrepareLauncher = Path.Combine(AppPath, "Configuration.exe");
         public static readonly string ReShadePath = Path.Combine(AppPath, "data", "reshade", "ReShade64.dll");
         public static readonly string InjectorPath = Path.Combine(AppPath, "data", "reshade", "inject64.exe");
         public static readonly string FpsUnlockerExePath = Path.Combine(AppPath, "data", "unlocker", "unlockfps_clr.exe");
         public static readonly string FpsUnlockerCfgPath = Path.Combine(AppPath, "data", "unlocker", "unlocker.config.json");
-        public static readonly IniFile Settings = new IniFile(Path.Combine(AppData, "settings.ini"));
         public static readonly Icon Ico = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
 
         // Web
@@ -49,8 +47,11 @@ namespace StellaLauncher
             return httpClient;
         });
 
-        // public static readonly string WebApi = Debugger.IsAttached ? "http://127.0.0.1:4010/api/v5" : "https://api.sefinek.net/api/v5";
-        public static readonly string WebApi = "https://api.sefinek.net/api/v5";
+        public static readonly string WebApi = Debugger.IsAttached ? "http://127.0.0.1:4010/api/v5" : "https://api.sefinek.net/api/v5";
+        //  public static readonly string WebApi = "https://api.sefinek.net/api/v5";
+
+        // Config
+        public static readonly IniFile Settings = new IniFile(Path.Combine(AppData, "settings.ini"));
 
         // Lang
         private static readonly string[] SupportedLangs = { "en", "pl" };
@@ -58,24 +59,21 @@ namespace StellaLauncher
         // Registry
         public static readonly string RegistryPath = @"Software\Stella Mod Launcher";
 
-        // Logger
-        public static Logger Logger = LogManager.GetCurrentClassLogger();
-
         // public static void DisposeHttpClient()
         // {
         //     if (WbClient.IsValueCreated) WbClient.Value.Dispose();
         // }
+
+        public static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         public static HttpClient SefinWebClient => WbClient.Value;
 
         [DllImport("user32.dll")]
         private static extern bool SetProcessDpiAwarenessContext(IntPtr dpiContext);
 
+        [STAThread]
         private static void Main()
         {
-            Logger = Logger.WithProperty("AppName", "Launcher");
-            Logger = Logger.WithProperty("AppVersion", AppVersion);
-
             // Set language
             string currentLang = Settings.ReadString("Language", "UI", null);
             bool isSupportedLanguage = SupportedLangs.Contains(currentLang);
@@ -102,10 +100,10 @@ namespace StellaLauncher
             if (Debugger.IsAttached) Logger.Debug($"CPU Serial Number {ComputerInfo.GetCpuSerialNumber()}");
 
 
-            if (Process.GetProcessesByName(AppNameVer).Length > 1)
+            if (Process.GetProcessesByName(AppName).Length > 1)
             {
                 Logger.Info("One instance is currently open.");
-                MessageBox.Show(string.Format(Resources.Program_SorryOneInstanceIsCurrentlyOpen_, Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly()?.Location)), AppNameVer, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show(string.Format(Resources.Program_SorryOneInstanceIsCurrentlyOpen_, Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly()?.Location)), AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 Environment.Exit(998765341);
             }
