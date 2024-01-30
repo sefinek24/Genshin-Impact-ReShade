828dcc402cf223f965e82b9b7ebe5747f920ce2f
Sefinek <contact@sefinek.net>
2024-01-30 17:52:01 +0100

    Change icon

--- Genshin Stella Mod/Properties/Resources.Designer.cs
+++ Genshin Stella Mod/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace GenshinStellaMod.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon nahida_icon {
-            get {
-                object obj = ResourceManager.GetObject("nahida_icon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
