0eaa50a5fdda67b7d480aa49d8e4867943861dcc
Sefinek <contact@sefinek.net>
2024-01-30 17:57:19 +0100

    Update DeleteBenefits.cs

--- Stella Mod Launcher/Scripts/Patrons/DeleteBenefits.cs
+++ Stella Mod Launcher/Scripts/Patrons/DeleteBenefits.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using StellaLauncher.Forms;
 
 namespace StellaLauncher.Scripts.Patrons
