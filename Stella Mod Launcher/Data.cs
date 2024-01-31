7fcb3d86cf194f0c14891a4ca43fba68633f5e3a
Sefinek <contact@sefinek.net>
2024-01-31 19:09:52 +0100

    Update Data.cs

--- Stella Mod Launcher/Data.cs
+++ Stella Mod Launcher/Data.cs
@@ -7,7 +7,7 @@ namespace StellaLauncher
 		public static readonly string Email = "contact@sefinek.net";
 
 		/* Versions */
-		public static readonly string ReShadeVer = "5.9.2";
+		public static readonly string ReShadeVer = "6.0.1";
 		public static readonly string UnlockerVer = "3.1.0";
 	}
 }
