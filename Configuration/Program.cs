f226dc4511f31ea9afdf3062de420b3c2a930bc4
Sefinek <contact@sefinek.net>
2024-01-31 19:35:01 +0100

    Update Program.cs

--- Configuration/Program.cs
+++ Configuration/Program.cs
@@ -8,13 +8,7 @@ namespace Configuration
 	internal static class Program
 	{
 		/// <summary>
-		///    The
-		///    main
-		///    entry
-		///    point
-		///    for
-		///    the
-		///    application.
+		///    The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		private static void Main()
