23a1b6c6254af7622e7c4f2bf03910f46181eaff
Sefinek <contact@sefinek.net>
2024-02-01 18:27:05 +0100

    WebView2 1.0.2210.55

--- Stella.Launcher/Forms/Other/WebView2Shake.Designer.cs
+++ Stella.Launcher/Forms/Other/WebView2Shake.Designer.cs
@@ -36,10 +36,10 @@ namespace StellaLauncher.Forms.Other
             // webView21
             // 
             this.webView21.AllowExternalDrop = true;
+            resources.ApplyResources(this.webView21, "webView21");
             this.webView21.BackColor = System.Drawing.Color.Black;
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.Transparent;
-            resources.ApplyResources(this.webView21, "webView21");
             this.webView21.Name = "webView21";
             this.webView21.ZoomFactor = 1D;
             // 
