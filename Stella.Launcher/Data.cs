ca8ee93837764352db1436b9b349b8ba7ff14f89
Sefinek <contact@sefinek.net>
2024-02-11 16:28:59 +0100

    Update Data.cs

--- Stella.Launcher/Data.cs
+++ Stella.Launcher/Data.cs
@@ -7,7 +7,7 @@ namespace StellaLauncher
 		public static readonly string Email = "contact@sefinek.net";
 
 		/* Versions */
-		public static readonly string ReShadeVer = "6.0.1";
-		public static readonly string UnlockerVer = "3.1.0";
+		public static readonly string ReShadeVer = "6.0.1"; // https://github.com/crosire/reshade
+		public static readonly string UnlockerVer = "3.2.0"; // https://github.com/sefinek24/Genshin-FPS-Unlocker
 	}
 }
