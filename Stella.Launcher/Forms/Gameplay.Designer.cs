e2f53d4fa9f4b070dc0b81306346e71c027bbef2
Sefinek <contact@sefinek.net>
2024-03-04 02:39:11 +0100

    Revert "Update forms"
    This reverts commit 9f39dd56c751fdfdd605ed4aec9262f33849e277.

--- Stella.Launcher/Forms/Gameplay.Designer.cs
+++ Stella.Launcher/Forms/Gameplay.Designer.cs
@@ -20,184 +20,185 @@ namespace StellaModLauncher.Forms
             base.Dispose(disposing);
         }
 
-		  #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		  /// <summary>
-		  /// Required method for Designer support - do not modify
-		  /// the contents of this method with the code editor.
-		  /// </summary>
-		  private void InitializeComponent()
-		  {
-				components = new System.ComponentModel.Container();
-				System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gameplay));
-				panel2 = new Panel();
-				panel1 = new Panel();
-				linkLabel1 = new LinkLabel();
-				label1 = new Label();
-				linkLabel2 = new LinkLabel();
-				linkLabel3 = new LinkLabel();
-				linkLabel4 = new LinkLabel();
-				label2 = new Label();
-				toolTip1 = new ToolTip(components);
-				pictureBox3 = new PictureBox();
-				pictureBox2 = new PictureBox();
-				webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
-				panel1.SuspendLayout();
-				((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-				((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-				((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
-				SuspendLayout();
-				// 
-				// panel2
-				// 
-				resources.ApplyResources(panel2, "panel2");
-				panel2.BackColor = Color.Transparent;
-				panel2.Cursor = Cursors.Hand;
-				panel2.ForeColor = Color.Transparent;
-				panel2.Name = "panel2";
-				toolTip1.SetToolTip(panel2, resources.GetString("panel2.ToolTip"));
-				panel2.Click += Exit_Click;
-				// 
-				// panel1
-				// 
-				panel1.BackColor = Color.Transparent;
-				resources.ApplyResources(panel1, "panel1");
-				panel1.Controls.Add(panel2);
-				panel1.Cursor = Cursors.SizeAll;
-				panel1.ForeColor = Color.Transparent;
-				panel1.Name = "panel1";
-				panel1.MouseDown += MouseDown_Event;
-				panel1.MouseMove += MouseMove_Event;
-				panel1.MouseUp += MouseUp_Event;
-				// 
-				// linkLabel1
-				// 
-				linkLabel1.ActiveLinkColor = Color.DodgerBlue;
-				resources.ApplyResources(linkLabel1, "linkLabel1");
-				linkLabel1.BackColor = Color.Transparent;
-				linkLabel1.Cursor = Cursors.Hand;
-				linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
-				linkLabel1.LinkColor = Color.White;
-				linkLabel1.Name = "linkLabel1";
-				linkLabel1.TabStop = true;
-				linkLabel1.LinkClicked += OpenInBrowser_LinkClicked;
-				// 
-				// label1
-				// 
-				resources.ApplyResources(label1, "label1");
-				label1.BackColor = Color.Transparent;
-				label1.ForeColor = Color.White;
-				label1.Name = "label1";
-				// 
-				// linkLabel2
-				// 
-				linkLabel2.ActiveLinkColor = Color.DodgerBlue;
-				resources.ApplyResources(linkLabel2, "linkLabel2");
-				linkLabel2.BackColor = Color.Transparent;
-				linkLabel2.Cursor = Cursors.Hand;
-				linkLabel2.LinkBehavior = LinkBehavior.HoverUnderline;
-				linkLabel2.LinkColor = Color.White;
-				linkLabel2.Name = "linkLabel2";
-				linkLabel2.TabStop = true;
-				toolTip1.SetToolTip(linkLabel2, resources.GetString("linkLabel2.ToolTip"));
-				linkLabel2.LinkClicked += Documentation_LinkLabel;
-				// 
-				// linkLabel3
-				// 
-				linkLabel3.ActiveLinkColor = Color.DodgerBlue;
-				resources.ApplyResources(linkLabel3, "linkLabel3");
-				linkLabel3.BackColor = Color.Transparent;
-				linkLabel3.Cursor = Cursors.Hand;
-				linkLabel3.LinkBehavior = LinkBehavior.HoverUnderline;
-				linkLabel3.LinkColor = Color.White;
-				linkLabel3.Name = "linkLabel3";
-				linkLabel3.TabStop = true;
-				toolTip1.SetToolTip(linkLabel3, resources.GetString("linkLabel3.ToolTip"));
-				linkLabel3.LinkClicked += Gallery_LinkLabel;
-				// 
-				// linkLabel4
-				// 
-				linkLabel4.ActiveLinkColor = Color.DodgerBlue;
-				resources.ApplyResources(linkLabel4, "linkLabel4");
-				linkLabel4.BackColor = Color.Transparent;
-				linkLabel4.Cursor = Cursors.Hand;
-				linkLabel4.LinkBehavior = LinkBehavior.HoverUnderline;
-				linkLabel4.LinkColor = Color.White;
-				linkLabel4.Name = "linkLabel4";
-				linkLabel4.TabStop = true;
-				toolTip1.SetToolTip(linkLabel4, resources.GetString("linkLabel4.ToolTip"));
-				linkLabel4.LinkClicked += Videos_LinkLabel;
-				// 
-				// label2
-				// 
-				resources.ApplyResources(label2, "label2");
-				label2.BackColor = Color.Transparent;
-				label2.Cursor = Cursors.Help;
-				label2.ForeColor = Color.White;
-				label2.Name = "label2";
-				// 
-				// pictureBox3
-				// 
-				resources.ApplyResources(pictureBox3, "pictureBox3");
-				pictureBox3.BackColor = Color.Transparent;
-				pictureBox3.Cursor = Cursors.Help;
-				pictureBox3.Image = Properties.Resources.uk_flag;
-				pictureBox3.Name = "pictureBox3";
-				pictureBox3.TabStop = false;
-				toolTip1.SetToolTip(pictureBox3, resources.GetString("pictureBox3.ToolTip"));
-				// 
-				// pictureBox2
-				// 
-				resources.ApplyResources(pictureBox2, "pictureBox2");
-				pictureBox2.BackColor = Color.Transparent;
-				pictureBox2.Cursor = Cursors.Help;
-				pictureBox2.Image = Properties.Resources.poland_flag;
-				pictureBox2.Name = "pictureBox2";
-				pictureBox2.TabStop = false;
-				toolTip1.SetToolTip(pictureBox2, resources.GetString("pictureBox2.ToolTip"));
-				// 
-				// webView21
-				// 
-				webView21.AllowExternalDrop = true;
-				resources.ApplyResources(webView21, "webView21");
-				webView21.BackColor = Color.Black;
-				webView21.CreationProperties = null;
-				webView21.DefaultBackgroundColor = Color.Transparent;
-				webView21.ForeColor = Color.Transparent;
-				webView21.Name = "webView21";
-				webView21.ZoomFactor = 1D;
-				// 
-				// Gameplay
-				// 
-				resources.ApplyResources(this, "$this");
-				AutoScaleMode = AutoScaleMode.Dpi;
-				BackColor = Color.Black;
-				BackgroundImage = Properties.Resources.bg_gameplay;
-				Controls.Add(linkLabel1);
-				Controls.Add(webView21);
-				Controls.Add(pictureBox3);
-				Controls.Add(pictureBox2);
-				Controls.Add(label2);
-				Controls.Add(linkLabel4);
-				Controls.Add(linkLabel3);
-				Controls.Add(linkLabel2);
-				Controls.Add(label1);
-				Controls.Add(panel1);
-				FormBorderStyle = FormBorderStyle.None;
-				Name = "Gameplay";
-				Load += Gameplay_Load;
-				Shown += Tutorial_Shown;
-				panel1.ResumeLayout(false);
-				((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-				((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-				((System.ComponentModel.ISupportInitialize)webView21).EndInit();
-				ResumeLayout(false);
-				PerformLayout();
-		  }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gameplay));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Name = "panel2";
+            this.toolTip1.SetToolTip(this.panel2, resources.GetString("panel2.ToolTip"));
+            this.panel2.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Name = "panel1";
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown_Event);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove_Event);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_Event);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OpenInBrowser_LinkClicked);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            resources.ApplyResources(this.linkLabel2, "linkLabel2");
+            this.linkLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel2.LinkColor = System.Drawing.Color.White;
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.TabStop = true;
+            this.toolTip1.SetToolTip(this.linkLabel2, resources.GetString("linkLabel2.ToolTip"));
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Documentation_LinkLabel);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            resources.ApplyResources(this.linkLabel3, "linkLabel3");
+            this.linkLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel3.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel3.LinkColor = System.Drawing.Color.White;
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.TabStop = true;
+            this.toolTip1.SetToolTip(this.linkLabel3, resources.GetString("linkLabel3.ToolTip"));
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Gallery_LinkLabel);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.ActiveLinkColor = System.Drawing.Color.DodgerBlue;
+            resources.ApplyResources(this.linkLabel4, "linkLabel4");
+            this.linkLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel4.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel4.LinkColor = System.Drawing.Color.White;
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.TabStop = true;
+            this.toolTip1.SetToolTip(this.linkLabel4, resources.GetString("linkLabel4.ToolTip"));
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Videos_LinkLabel);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Name = "label2";
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Help;
+            this.pictureBox3.Image = global::StellaModLauncher.Properties.Resources.uk_flag;
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox3, resources.GetString("pictureBox3.ToolTip"));
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Help;
+            this.pictureBox2.Image = global::StellaModLauncher.Properties.Resources.poland_flag;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox2, resources.GetString("pictureBox2.ToolTip"));
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            resources.ApplyResources(this.webView21, "webView21");
+            this.webView21.BackColor = System.Drawing.Color.Black;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.Transparent;
+            this.webView21.ForeColor = System.Drawing.Color.Transparent;
+            this.webView21.Name = "webView21";
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Gameplay
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::StellaModLauncher.Properties.Resources.bg_gameplay;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Gameplay";
+            this.Load += new System.EventHandler(this.Gameplay_Load);
+            this.Shown += new System.EventHandler(this.Tutorial_Shown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-		  #endregion
+        }
 
-		  private System.Windows.Forms.Panel panel2;
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel2;
