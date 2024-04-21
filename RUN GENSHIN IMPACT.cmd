c791e51e185e761b5e942be42e98f52f9fc68090
Sefinek Actions <contact@sefinek.net>
2024-04-21 02:46:31 +0200

    https://stella.sefinek.net

--- RUN GENSHIN IMPACT.cmd
+++ RUN GENSHIN IMPACT.cmd
@@ -7,7 +7,7 @@ echo If you used the 'git clone [Repository Link]' command, you have downloaded 
 echo Compiling the code on your machine might not be feasible, as certain files are not publicly accessible
 echo due to security reasons.
 echo.
-echo To install the Genshin Stella Mod, please visit https://genshin.sefinek.net and click on the 'Download' button.
+echo To install the Genshin Stella Mod, please visit https://stella.sefinek.net and click on the 'Download' button.
 echo.
 echo Additionally, be aware that the YouTube video titled
 echo "How to Install Genshin Impact Reshade Mod for Enhanced Graphics Quality and Unlocked Frame Rates? 2022"
