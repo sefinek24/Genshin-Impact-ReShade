d19b7032283f9412410dcb038c78024abba59012
Sefinek <contact@sefinek.net>
2024-09-01 20:55:24 +0200

    OPEN THE SOLUTION.cmd

--- OPEN THE SOLUTION.cmd
+++ OPEN THE SOLUTION.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "file_path=C:\GitHub\VS\Stella\Genshin-Stella-Mod\Genshin Stella Mod made by Sefinek.sln"
+set "file_path=D:\Projects\stella\vs\Genshin-Impact-ReShade\Genshin Stella Mod made by Sefinek.sln"
 set "vs_path=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
 
 echo Running Genshin Stella Mod made by Sefinek.sln file as administrator... && echo.
