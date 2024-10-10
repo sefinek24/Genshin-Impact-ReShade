366780577af37804343afc5ecea4d9e03d6a014d
Sefinek <contact@sefinek.net>
2024-10-10 05:39:34 +0200

    Update OPEN THE SOLUTION.cmd

--- OPEN THE SOLUTION.cmd
+++ OPEN THE SOLUTION.cmd
@@ -5,7 +5,7 @@ set "file_path=D:\Projects\stella\Genshin-Impact-ReShade\Genshin Stella Mod made
 set "vs_path=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
 
 for %%f in ("%file_path%") do (
-    echo Running the %%~nxf file as administrator... && echo.
+    echo Running the '%%~nxf' file as administrator... && echo.
     powershell.exe -Command "Start-Process '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
 )
 
