bfb28f5813d818854afae2055f4ffa9ce9a3a3b5
Sefinek <contact@sefinek.net>
2024-10-10 05:39:08 +0200

    Update OPEN THE SOLUTION.cmd

--- OPEN THE SOLUTION.cmd
+++ OPEN THE SOLUTION.cmd
@@ -4,8 +4,9 @@ setlocal
 set "file_path=D:\Projects\stella\Genshin-Impact-ReShade\Genshin Stella Mod made by Sefinek.sln"
 set "vs_path=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
 
-echo Running Genshin Stella Mod made by Sefinek.sln file as administrator... && echo.
-
-powershell.exe -Command "Start-Process -FilePath '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
+for %%f in ("%file_path%") do (
+    echo Running the %%~nxf file as administrator... && echo.
+    powershell.exe -Command "Start-Process '%vs_path%' -ArgumentList '\"%file_path%\"' -Verb RunAs"
+)
 
 endlocal
